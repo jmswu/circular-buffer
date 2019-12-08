9b26d0a5cf66d31ff5ada7e26fa5ccde997ca869
James Wu <jameswu62@yahoo.co.nz>
2019-12-08 20:23:59 +1300

    update CBUFF_construct()

--- cbuff.c
+++ cbuff.c
@@ -1,10 +1,15 @@
 #include "cbuff.h"
 
 CBUFF_Handle CBUFF_construct(volatile CBUFF_Struct *cbuff, volatile uint8_t *data, uint16_t size){
+
+    /* make sure pointers are valid */
+    if (cbuff == 0) return 0;
+    if (data == 0) return 0;
+
     cbuff->capacity = size;
-    cbuff->count = 0;
+    cbuff->count = 0U;
     cbuff->data = data;
-    cbuff->head = 0;
-    cbuff->tail=0;
+    cbuff->head = 0U;
+    cbuff->tail=0U;
     return (CBUFF_Handle)cbuff;
 }
