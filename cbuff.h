3b7854808bc979fa53e56618e140c70247399a4a
James Wu <jameswu62@yahoo.co.nz>
2019-12-12 20:49:26 +1300

    change comment style

--- cbuff.h
+++ cbuff.h
@@ -8,8 +8,8 @@ extern "C" {
 #include <stdint.h>
 #include <string.h>
 
-/* User need to insert codes that apply to the archetechure 
- * that will disable global interrupt
+/*! User need to insert codes that apply to the archetechure 
+ *! that will disable global interrupt
  */
 #define CBUFF_CRITICAL_SECTION_BEGIN()      do{/* user code here */}while(0)
 #define CBUFF_CRITICAL_SECTION_END()        do{/* user code here */}while(0)
@@ -24,46 +24,47 @@ typedef struct CBUFF_STRUCT{
 
 typedef CBUFF_Struct *CBUFF_Handle;
 
-/* Construct the initialize the ring buffer
- * @param volatile CBUFF_Struct *cbuff      - pointer to the ring buffer structure
- * @param volatile uint8_t *data            - pointer to the ring buffer memory location
- * @param uint16_t size                     - the maximum capacity of the ring buffer
- * @return CBUFF_Handle                     - handle to ring buffer
+/*! \brief Construct the initialize the ring buffer
+ *! \param volatile CBUFF_Struct *cbuff      - pointer to the ring buffer structure
+ *! \param volatile uint8_t *data            - pointer to the ring buffer memory location
+ *! \param uint16_t size                     - the maximum capacity of the ring buffer
+ *! \return CBUFF_Handle                     - handle to ring buffer
  */
 CBUFF_Handle CBUFF_construct(volatile CBUFF_Struct *cbuff, volatile uint8_t *data, uint16_t size);
 
-/* Put one byte into the circular buffer
- * @param CBUFF_Handle handle               - circular buffer handler
- * @param uint8_t data                      - one byte that need to be put into the buffer
- * @return                                  - void
+/*! \brief  Put one byte into the circular buffer
+ *! \param CBUFF_Handle handle               - circular buffer handler
+ *! \param uint8_t data                      - one byte that need to be put into the buffer
+ *! \return                                  - void
  */
 void CBUFF_put(CBUFF_Handle handle, uint8_t data);
 
-/* Get one byte from the circular buffer
- * @param CBUFF_Handle handle               - circular buffer handler
- * @return uint8_t                          - one byte read from the circular buffer
+/*! \brief Get one byte from the circular buffer
+ *! \param CBUFF_Handle handle               - circular buffer handler
+ *! \return uint8_t                          - one byte read from the circular buffer
  */
 uint8_t CBUFF_get(CBUFF_Handle handle);
 
-/* Get one byte from the circular buffer without actually remove it from the ring buffer
- * @param CBUFF_Handle handle               - circular buffer handler
- * @return uint8_t                          - one byte read from the circular buffer
+/*! \brief Get one byte from the circular buffer without actually remove it from the ring buffer
+ *! \param CBUFF_Handle handle               - circular buffer handler
+ *! \return uint8_t                          - one byte read from the circular buffer
  */
 uint8_t CBUFF_peek(CBUFF_Handle handle);
 
-/* Tell if the ring buffer is full
- * @param CBUFF_Handle handle               - circular buffer handler
- * @return int                              - 1 = is full, 0 = is not full
+/*! \brief Tell if the ring buffer is full
+ *! \param CBUFF_Handle handle               - circular buffer handler
+ *! \return int                              - 1 = is full, 0 = is not full
  */
 int CBUFF_isFull(CBUFF_Handle handle);
 
-/* Tell if the ring buffer is empty
- * @param CBUFF_Handle handle               - circular buffer handler
- * @return int                              - 1 = is empty, 0 = is not empty
+/*! \brief Tell if the ring buffer is empty
+ *! \param CBUFF_Handle handle               - circular buffer handler
+ *! \return int                              - 1 = is empty, 0 = is not empty
  */
 int CBUFF_isEmpty(CBUFF_Handle handle);
 
-
+/*! Struture for constructing a object ring buffer
+ */
 typedef struct CBUFF_OBJ_STRUCT{
     volatile void *data;
     volatile uint16_t head;
@@ -75,46 +76,46 @@ typedef struct CBUFF_OBJ_STRUCT{
 
 typedef CBUFF_OBJ_Struct *CBUFF_OBJ_Handle;
 
-/* Construct the initialize the ring buffer
- * @param volatile CBUFF_OBJ_Struct *cbuff      - pointer to the object ring buffer structure
- * @param volatile uint8_t *data                - pointer to the ring buffer memory location
- * @param uint16_t obj_size                     - size in byte of the object
- * @param uint16_t capacity                     - Maximum number of object that can be stored in the buffer
- * @return CBUFF_OBJ_Handle                     - handle to object ring buffer
+/*! \brief Construct the initialize the ring buffer
+ *! \param volatile CBUFF_OBJ_Struct *cbuff      - pointer to the object ring buffer structure
+ *! \param volatile uint8_t *data                - pointer to the ring buffer memory location
+ *! \param uint16_t obj_size                     - size in byte of the object
+ *! \param uint16_t capacity                     - Maximum number of object that can be stored in the buffer
+ *! \return CBUFF_OBJ_Handle                     - handle to object ring buffer
  */
 CBUFF_OBJ_Handle CBUFF_OBJ_construct(volatile CBUFF_OBJ_Struct *cbuff, volatile uint8_t *data, uint16_t obj_size, uint16_t capacity);
 
-/* Put one object into the circular buffer
- * @param CBUFF_OBJ_Handle handle           - object circular buffer handler
- * @param void *obj                         - pointer to the object that need to be put into the buffer
- * @return                                  - void
+/*! \brief Put one object into the circular buffer
+ *! \param CBUFF_OBJ_Handle handle           - object circular buffer handler
+ *! \param void *obj                         - pointer to the object that need to be put into the buffer
+ *! \return                                  - void
  */
 void CBUFF_OBJ_put(CBUFF_OBJ_Handle handle, void *obj);
 
-/* Get one object from the circular buffer
- * @param CBUFF_OBJ_Handle handle           - circular buffer handler
- * @param void *obj                         - pointer to the object that CBUFF_OBJ_get() write to
- * @return                                  - void
+/*! \brief Get one object from the circular buffer
+ *! \param CBUFF_OBJ_Handle handle           - circular buffer handler
+ *! \param void *obj                         - pointer to the object that CBUFF_OBJ_get() write to
+ *! \return                                  - void
  */
 void CBUFF_OBJ_get(CBUFF_OBJ_Handle handle, void *obj);
 
-/* Get one object from the circular buffer without actually remove it from the ring buffer
- * @param CBUFF_OBJ_Handle handle           - circular buffer handler
- * @param void *obj                         - pointer to the object that CBUFF_OBJ_get() write to
- * @return                                  - void
+/*! \brief Get one object from the circular buffer without actually remove it from the ring buffer
+ *! \param CBUFF_OBJ_Handle handle           - circular buffer handler
+ *! \param void *obj                         - pointer to the object that CBUFF_OBJ_get() write to
+ *! \return                                  - void
  */
 void CBUFF_OBJ_peek(CBUFF_OBJ_Handle handle, void *obj);
 
 
-/* Tell if the ring buffer is full
- * @param CBUFF_OBJ_Handle handle           - object circular buffer handler
- * @return int                              - 1 = is full, 0 = is not full
+/*! \brief Tell if the ring buffer is full
+ *! \param CBUFF_OBJ_Handle handle           - object circular buffer handler
+ *! \return int                              - 1 = is full, 0 = is not full
  */
 int CBUFF_OBJ_isFull(CBUFF_OBJ_Handle handle);
 
-/* Tell if the ring buffer is empty
- * @param CBUFF_OBJ_Handle handle           - object circular buffer handler
- * @return int                              - 1 = is empty, 0 = is not empty
+/*! \brief Tell if the ring buffer is empty
+ *! \param CBUFF_OBJ_Handle handle           - object circular buffer handler
+ *! \return int                              - 1 = is empty, 0 = is not empty
  */
 int CBUFF_OBJ_isEmpty(CBUFF_OBJ_Handle handle);
 
--- example.c
+++ example.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "cbuff.h"
 
+
+/* data structr for testing object ring buffer */
 typedef struct{
     uint8_t data1;
     uint8_t data2;
@@ -10,11 +12,13 @@ typedef struct{
 int main(char *argv[], int argc){
 
     printf("Testing circular buffer!\n");
-    volatile CBUFF_Struct cbuff;
-    volatile uint8_t data[10];
+    volatile CBUFF_Struct cbuff;    /* ring buffer structure    */
+    volatile uint8_t data[10];      /* ring buffer memory       */
 
+    /* construct ring buffer */
     CBUFF_Handle handle = CBUFF_construct(&cbuff, data, sizeof(data)/sizeof(data[0]));
 
+    /* put some data into the ring buffer */
     if (!CBUFF_isFull(handle)) CBUFF_put(handle, 'H');
     if (!CBUFF_isFull(handle)) CBUFF_put(handle, 'E');
     if (!CBUFF_isFull(handle)) CBUFF_put(handle, 'L');
@@ -22,18 +26,22 @@ int main(char *argv[], int argc){
     if (!CBUFF_isFull(handle)) CBUFF_put(handle, 'O');
     if (!CBUFF_isFull(handle)) CBUFF_put(handle, 'W');
 
+    /* print everything out in the ring buffer */
     while (!CBUFF_isEmpty(handle)){
         printf("%c", CBUFF_get(handle));
     }
 
+    /* testing object ring buffer */
     printf("\n");
     printf("-------------------------\n");
     printf("Testing circular object buffer!\n");
-    volatile CBUFF_OBJ_Struct cbuff_obj;
-    volatile uint8_t data_obj[10 * sizeof(test_t)];
+    volatile CBUFF_OBJ_Struct cbuff_obj;            /* object ring buffer structure     */
+    volatile uint8_t data_obj[10 * sizeof(test_t)]; /* object ring buffer memory        */
 
+    /* construct ring buffer */
     CBUFF_OBJ_Handle handle_obj = CBUFF_OBJ_construct(&cbuff_obj, data_obj, sizeof(test_t), sizeof(data_obj)/sizeof(test_t));
 
+    /* put object into ring buffer */
     test_t test;
     for(int i = 0; i < 20; i++){
         test.data1 = i * 1;
@@ -43,10 +51,9 @@ int main(char *argv[], int argc){
         printf("test: %d, %d, %d\n", test.data1, test.data2, test.data3);
         printf("iteration: %d, isEmpty: %d\n", i, CBUFF_OBJ_isEmpty(handle_obj));
         printf("iteration: %d, isFull : %d\n", i, CBUFF_OBJ_isFull(handle_obj));
-        
     }
 
-
+    /* print every thing out from the ring buffer */
     while (!CBUFF_OBJ_isEmpty(handle_obj)){
         CBUFF_OBJ_get(handle_obj, &test);
         printf("test: %d, %d, %d\n", test.data1, test.data2, test.data3);
