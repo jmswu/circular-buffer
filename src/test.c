f3d1852428f6641a75c7a8acd4ef21d1e0d504fe
James Wu <jameswu62@yahoo.co.nz>
2021-02-04 11:22:40 +1300

    add TEST_REMOVE_DATA()

--- src/test.c
+++ src/test.c
@@ -6,6 +6,7 @@ void TEST_HANDLE_CREATION(void);
 void TEST_ADD_DATA(void);
 void TEST_ADD_DATA_BUFFER_FULL(void);
 void TEST_ADD_DATA_OVERFLOW(void);
+void TEST_REMOVE_DATA(void);
 
 int main(int argc, char ** argv){
 
@@ -17,6 +18,7 @@ int main(int argc, char ** argv){
     TEST_ADD_DATA();
     TEST_ADD_DATA_BUFFER_FULL();
     TEST_ADD_DATA_OVERFLOW();
+    TEST_REMOVE_DATA();
 
     printf("done!\n\r");
 
@@ -68,6 +70,40 @@ void TEST_ADD_DATA(void)
     TEST_ASSERT_EQUAL(0, handle->tail);
 }
 
+void TEST_REMOVE_DATA(void)
+{
+    const uint16_t BUFFER_SIZE = 30;
+    volatile uint8_t buffer[BUFFER_SIZE];
+    volatile CBUFF_Struct bufferStruct;
+    volatile CBUFF_Handle handle = CBUFF_construct(&bufferStruct, buffer, BUFFER_SIZE);
+
+    uint8_t test_data1 = 42;
+    uint8_t test_data2 = 100;
+
+    CBUFF_put(handle, test_data1);
+    //TEST_ASSERT_EQUAL(1, handle->count);
+    //TEST_ASSERT_EQUAL(test_data1, handle->data[0]);
+    //TEST_ASSERT_EQUAL(1, handle->head);
+    //TEST_ASSERT_EQUAL(0, handle->tail);
+
+    CBUFF_put(handle, test_data2);
+    //TEST_ASSERT_EQUAL(2, handle->count);
+    //TEST_ASSERT_EQUAL(test_data1, handle->data[0]);
+    //TEST_ASSERT_EQUAL(test_data2, handle->data[1]);
+    //TEST_ASSERT_EQUAL(2, handle->head);
+    //TEST_ASSERT_EQUAL(0, handle->tail);
+
+    TEST_ASSERT_EQUAL(test_data1, CBUFF_get(handle));
+    TEST_ASSERT_EQUAL(1, handle->count);
+    TEST_ASSERT_EQUAL(1, handle->tail);
+    TEST_ASSERT_EQUAL(2, handle->head);
+
+    TEST_ASSERT_EQUAL(test_data2, CBUFF_get(handle));
+    TEST_ASSERT_EQUAL(0, handle->count);
+    TEST_ASSERT_EQUAL(2, handle->tail);
+    TEST_ASSERT_EQUAL(2, handle->head);
+}
+
 void TEST_ADD_DATA_BUFFER_FULL(void)
 {
     const uint16_t BUFFER_SIZE = 0xFFFF;
@@ -102,12 +138,13 @@ void TEST_ADD_DATA_OVERFLOW(void)
 
     TEST_ASSERT_EQUAL(BUFFER_SIZE, handle->count);
     TEST_ASSERT_EQUAL(0, handle->head);
+    TEST_ASSERT_EQUAL(BUFFER_SIZE - 1, handle->data[BUFFER_SIZE - 1]);
 
     // put another one more in the buffer
     CBUFF_put(handle, 10);
     TEST_ASSERT_EQUAL(BUFFER_SIZE, handle->count);
     TEST_ASSERT_EQUAL(0, handle->head);
-
+    TEST_ASSERT_EQUAL(BUFFER_SIZE - 1, handle->data[BUFFER_SIZE - 1]);
 }
 
 void setUp(void)
