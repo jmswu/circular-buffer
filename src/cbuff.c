aa8351e2718e22be88e3fc3bff4d723d8e8825b7
James Wu <jameswu62@yahoo.co.nz>
2021-02-11 09:13:44 +1300

    update version number

--- src/cbuff.c
+++ src/cbuff.c
@@ -2,7 +2,7 @@
 
 #define VER_MAJOR   (0U)
 #define VER_MINOR   (1U)
-#define VER_PATCH   (1U)
+#define VER_PATCH   (2U)
 #define VER_NUM     ((VER_MAJOR << 16) + (VER_MINOR << 8) + (VER_PATCH))
 
 uint32_t CBUFF_getVerNumber(void){
